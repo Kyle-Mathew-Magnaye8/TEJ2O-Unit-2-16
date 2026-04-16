7d573fcfedcd82ed53316409d6bba11e7d4ceee7
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2026-04-16 12:43:14 -0400

    changed code

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Kyle Matthew
+ * Created on: Apr 2026
+ * This program uses the radios including the distance sensor, 
+ * that if an object comes too close to the distance sensor attached to 1 Micro:Bit, 
+ * it recives the message to the other Micro:Bit.
 */
 
-basic.showString('Hello, World!')
+// setup
+radio.setGroup(6767)
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+  basic.showIcon(IconNames.Triangle)
+  radio.sendString("Hello, World!")
+  basic.showIcon(IconNames.Happy)
+})
+
+radio.onReceivedString(function (receivedString) {
+  basic.clearScreen()
+  basic.showString(receivedString)
+  basic.showIcon(IconNames.Happy)
+})
