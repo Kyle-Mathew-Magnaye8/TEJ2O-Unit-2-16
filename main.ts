aa511edcb3b3756442505a77234d798d970075e1
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2026-04-24 12:58:36 -0400

    changed comment

--- main.ts
+++ main.ts
@@ -4,14 +4,14 @@
  * Created on: Apr 2026
  * This program uses the radios including the distance sensor, 
  * that if an object comes too close to the distance sensor attached to 1 Micro:Bit, 
- * it recives the message to the other Micro:Bit.
+ * it receives the message to the other Micro:Bit.
 */
 
 // setup
 radio.setGroup(41)
 basic.showIcon(IconNames.Happy)
 
-// recives and shows strings from other Micro:bits
+// receives and shows strings from other Micro:bits
 radio.onReceivedString(function (receivedString) {
   basic.clearScreen()
   basic.showString(receivedString)
